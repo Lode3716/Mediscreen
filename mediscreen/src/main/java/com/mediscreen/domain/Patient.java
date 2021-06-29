f9d02a877d8ab21b40a56e69acc66a69dfb4e2bd
Laurent <lopadefe@gmail.com>
2021-06-29 23:22:07 +0200

    Update Pom reporting and application properties

--- mediscreen/src/main/java/com/mediscreen/domain/Patient.java
+++ mediscreen/src/main/java/com/mediscreen/domain/Patient.java
@@ -1,13 +1,15 @@
 package com.mediscreen.domain;
 
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDate;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @Entity
 @Table(name = "patient")
